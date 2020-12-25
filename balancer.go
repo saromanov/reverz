9576f248aaca6584da3b405cfe745d3b1bfbb976
Sergey <xxsmotur@gmail.com>
2020-12-25 23:50:30 +0500

    implementation of round robin

--- balancer.go
+++ balancer.go
@@ -1,6 +1,23 @@
 package reverz
 
+import (
+	"net/url"
+	"sync/atomic"
+)
+
 // Balancer provides implementation of balancing
 type Balancer interface {
-	Next() string
+	Next() *url.URL
+}
+
+// RoundRobin provides implementation of round robin algorithms 
+type RoundRobin struct {
+	urls []*url.URL
+	next uint32
+}
+
+// Next select teh next host
+func (r *RoundRobin) Next() *url.URL {
+	n := atomic.AddUint32(&r.next, 1)
+	return r.urls[(int(n)-1)%len(r.urls)]
 }
